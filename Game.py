159f6c30ddc996751a1a770a52a4a21b3108d874
Shubham Prasad <shubhamprasad63@gmail.com>
2017-09-10 18:40:30 +0530

    Taking input from user tested

--- Game.py
+++ Game.py
@@ -15,6 +15,32 @@ class Game:
         marker2 -- marker of second player
         '''
 
-        self.board = Board()
-        self.player1 = Player(player1, marker1)
-        self.player2 = Player(player2, marker2)
+        self.board = Board.Board()
+        self.player1 = Player.Player(player1, marker1)
+        self.player2 = Player.Player(player2, marker2)
+        self.player1_turn = True  # False, for player2 turn
+
+    def play(self):
+        ''' This method simulates the game play. '''
+
+        print('Enter position to mark: ')
+        row, col = input().split()
+        print(row, col)
+
+
+def main():
+
+    print('Enter name of first player (has marker \'x\') : ')
+    player1 = input()
+    marker1 = 'x'
+    print('Enter name of second player (has marker \'o\') : ')
+    player2 = input()
+    marker2 = 'o'
+
+    # Initialize game
+    game = Game(player1, player2, marker1, marker2)
+    game.play()
+
+
+if __name__ == '__main__':
+    main()
