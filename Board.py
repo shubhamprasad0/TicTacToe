b29acbdcc8a4261f5bd34601bd81111374f92292
Shubham Prasad <shubhamprasad63@gmail.com>
2017-09-10 20:23:39 +0530

    Game play method complete, two players can now play game

--- Board.py
+++ Board.py
@@ -8,3 +8,57 @@ class Board:
         self.board = [['_', '_', '_'],
                       ['_', '_', '_'],
                       ['_', '_', '_']]
+
+    def show_board(self):
+        for row in self.board:
+            print(row[0], row[1], row[2])
+
+    def is_full(self):
+        ''' This method checks if there is any vacant position left on the
+        board or not.  If there is any vacant position left, it returns True,
+        else it returns False.
+        '''
+
+        full = True
+        for row in self.board:
+            for position in row:
+                if position == '_':
+                    full = False
+                    break
+            if full is False:
+                break
+
+        return full
+
+    def winning_position(self):
+        ''' This method checks if the board is in winning position.
+        If the board is in winning position , it returns the marker of
+        the player who has won.  If the board is not in a winning position,
+        it returns None.
+        '''
+
+        # Check each row
+        for row in self.board:
+            if (row[0] != '_' and row[0] == row[1] and row[0] == row[2]):
+                return row[0]
+
+        # Check each column
+        board_transpose = list(zip(*self.board))
+        for col in board_transpose:
+            if (col[0] != '_' and col[0] == col[1] and col[0] == col[2]):
+                return col[0]
+
+        # Check main diagonal (top-left to bottom-right)
+        if (self.board[0][0] != '_' and
+                self.board[0][0] == self.board[1][1] and
+                self.board[0][0] == self.board[2][2]):
+            return self.board[0][0]
+
+        # Check other diagonal (top-right to bottom-left)
+        if (self.board[0][2] != '_' and
+                self.board[0][2] == self.board[1][1] and
+                self.board[0][2] == self.board[2][0]):
+            return self.board[0][2]
+
+        # The board is not in a winning position, so return None
+        return None
--- Game.py
+++ Game.py
@@ -19,13 +19,52 @@ class Game:
         self.player1 = Player.Player(player1, marker1)
         self.player2 = Player.Player(player2, marker2)
         self.player1_turn = True  # False, for player2 turn
+        self.winner = None
 
     def play(self):
         ''' This method simulates the game play. '''
 
-        print('Enter position to mark: ')
-        row, col = input().split()
-        print(row, col)
+        while self.board.is_full() is False:
+            print('_'*40)
+            self.board.show_board()
+            print('{}\'s Turn'.format(self.player1.name)
+                  if self.player1_turn is True else
+                  '{}\'s Turn'.format(self.player2.name))
+            print('Enter position to mark (0 based indexing): ')
+            row, col = [int(i) for i in input().split()]
+
+            # Check if the position entered is vacant or not
+            if self.board.board[row][col] != '_':
+                print('Entered position is already occupied, try again!')
+                continue
+
+            if self.player1_turn is True:
+                # player1's turn
+                self.player1.moves.append((row, col))
+                self.board.board[row][col] = 'x'
+                self.player1_turn = False
+            else:
+                # player2's turn
+                self.player2.moves.append((row, col))
+                self.board.board[row][col] = 'o'
+                self.player1_turn = True
+
+            verdict = self.board.winning_position()
+
+            if verdict is not None:
+                self.winner = self.player1 if verdict == 'x' else self.player2
+                break
+
+        # Show board at the end of the game
+        print('Game Over')
+        print('_' * 40)
+        self.board.show_board()
+
+        # Declare result of the game
+        if self.winner is not None:
+            print('Winner is {}'.format(self.winner.name))
+        else:
+            print('This is a draw')
 
 
 def main():
